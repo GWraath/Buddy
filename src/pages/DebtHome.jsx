555be6918f49d5786f8615b54783656bedbbe0be
GWraath <gar.wootton@gmail.com>
2023-09-08 16:25:46 +1200

    Attempted useAxios customhook - kindofworks

--- src/pages/DebtHome.jsx
+++ src/pages/DebtHome.jsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import { Button,CssBaseline, Box, Typography, 
-  Container, Link } from '@mui/material';
-import { useEffect, useState, useContext, useReducer } from 'react';
+import { CssBaseline, Container } from '@mui/material';
+import { useEffect, useContext } from 'react';
+import { useAxios } from '../hooks/useAxios';
 import { DebtContext } from '../context/DebtContext';
 import { PageTypeContext } from '../context/PageTypeContext'
 import { SearchContext } from '../context/SearchContext'
-import { useNavigate } from "react-router-dom";
 import FilterComponent from '../components/transactions/FilterComponent';
 import Axios from '../axios/Axios';
 
@@ -13,25 +12,19 @@ export default function debtHome() {
   const { pageType, setPageType } = useContext(PageTypeContext);
   const { debts, setDebts } = useContext(DebtContext);
   const { query } = useContext(SearchContext);
-  // const [state, dispatch] = useLogic()
   // const debtsPerPage = 6;
   const currentUserString = localStorage.getItem('currentUser');
   const currentUser = JSON.parse(currentUserString);
-  let navigate = useNavigate();
+
+  const transData = useAxios(
+    'get', 'debts', query
+  );
+  console.log(transData)
 
   // //get the debts
   useEffect(() => {
     setPageType('debts')
 }, [])
-  
-  // //if non user clicks delete, redirect to pna
-  // const doNotProceed = () => {
-  //   if (currentUser === null) {
-  //     navigate('/pna');
-  //   }
-  // }
-  // //call the function
-  // doNotProceed()
 
   return (
     <>
